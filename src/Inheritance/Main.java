508f2e0d465afb8ec1d402a60618f1ffe5b85349
Ruma <matteoh8@hotmail.it>
2025-03-07 17:58:01 +0100

    .

--- src/Inheritance/Main.java
+++ src/Inheritance/Main.java
@@ -14,43 +14,76 @@ public class Main {
 				return "Spawna un Gerry Scotti selvatico!\n" + super.toString();
 			}
 		};
-		System.out.println(gerryScotti);
+		System.out.println(gerryScotti);;
+		
+		Persona nomeLungo = new Persona("questoNomeE'TroppoLungo", "cognome", 100);
+        System.out.println(nomeLungo);
+
+        Persona elonMusk = new Persona("nome", "cognome", Integer.MAX_VALUE);
+        System.out.println(elonMusk);
 	}
 }
 
 class Persona {
-	private String name;
-	private String lastname;
-	private Integer money;
+	// Inizializzazioni di default
+	private String name = "";
+	private String lastname = "";
+	private Integer money = 0;
 
 	public Persona(String name, String lastname, Integer money) {
-		this.name = name;
-		this.lastname = lastname;
-		this.money = money;
+		// Assegnamo alle variabili di istanza i valori dai setter
+		// Essendo i setter delle funzioni, possiamo aggiungere la logica che vogliamo
+		// Soprattutto aggiungere controlli e verifiche.
+		
+		String tempName = setName(name);
+        String tempLastname = setLastname(lastname);
+        Integer tempMoney = setMoney(money + 100);
+		
+		if (tempName != null) {
+            this.name = tempName;
+        }
+        if (tempLastname != null) {
+            this.lastname = tempLastname;
+        }
+        if (tempMoney != null) {
+            this.money = tempMoney;
+        }
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public String setName(String name) {
+		if (name.length() > 20) {
+			System.out.println("Troppi caratteri! Massimo 20!");
+			return null;
+		}
+		return name;
 	}
 
 	public String getLastname() {
 		return lastname;
 	}
 
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
+	public String setLastname(String lastname) {
+		if (lastname.length() > 20) {
+			System.out.println("Troppi caratteri! Massimo 20!");
+			return null;
+		}
+		return lastname;
 	}
 
 	public Integer getMoney() {
 		return money;
 	}
 
-	public void setCF(Integer money) {
-		this.money = money;
+	public Integer setMoney(Integer money) {
+		if (money > Integer.MAX_VALUE) {
+			System.out.println("Bro, se fossi Elon Musk non faresti il programmatore");
+			return null;
+		}
+		return money;
 	}
 	
 	@Override
@@ -66,6 +99,7 @@ class CarloConti extends Persona {
 		super(name, lastname, money);
 	}
 	
+	// No problem, lo richiamiamo da un metodo statico pubblico
 	public static CarloConti creaCarloConti() {
 		return new CarloConti("Carlo", "Conti", 9999999);
 	}
